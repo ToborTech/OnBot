66ca833f617b118a3ef6655650841198fadb2d81
Alpha-kun <CharlieLiuAlpha@gmail.com>
2019-10-16 23:00:58 -0700

    added simple autonomous program(drive a quare)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Template_Auto.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous(name = "ToBot: autonomous", group = "ToBot")
+public class Template_Auto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        HardwareToBot robot = new HardwareToBot();   // Use a ToBot's hardware
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap, telemetry);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        //TODO write your auto codes here
+        for (int i = 0; i < 4; i++) {
+            robot.forward(0.5, 20, 5000);
+            robot.turn(0.5, 90, 4000);
+        }
+    }
+}
